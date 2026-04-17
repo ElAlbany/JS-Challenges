a87b456dba41bd5ecfbef25b8e4d624b5a1ae8e1
ElAlbany <zajdhany112@gmail.com>
2026-04-17 16:27:27 +0200

    at adding keydown event

--- typing-speed-test-main/script.js
+++ typing-speed-test-main/script.js
@@ -4,30 +4,45 @@ const dropdownmenu1 = ccombo1.querySelector(".dropdown-menu");
 const diff2 = document.querySelector("#difficulty2");
 const ccombo2 = diff2.closest(".custom-combo");
 const dropdownmenu2 = ccombo2.querySelector(".dropdown-menu");
-
 const options = document.querySelectorAll(".option");
+const restartBtn = document.querySelector(".restart");
+const textContainer = document.querySelector(".text-container");
+const text = document.querySelector(".text");
+const start = document.querySelector(".start-test");
+const bestScoreNumber = document.querySelector("#best-score-number");
+
+let easyTexts = [];
+let mediumTexts = [];
+let hardTexts = [];
+let easyNum = 1;
+let mediumNum = 1;
+let hardNum = 1;
 
 diff1.addEventListener("click", () => {
-  if (dropdownmenu2) {
-    if (dropdownmenu2.classList.contains("open"))
-      dropdownmenu2.classList.remove("open");
-  }
-  if (dropdownmenu1) {
-    if (!dropdownmenu1.classList.contains("open"))
-      dropdownmenu1.classList.add("open");
-    else dropdownmenu1.classList.remove("open");
+  if (restartBtn.classList.contains("hide")) {
+    if (dropdownmenu2) {
+      if (dropdownmenu2.classList.contains("open"))
+        dropdownmenu2.classList.remove("open");
+    }
+    if (dropdownmenu1) {
+      if (!dropdownmenu1.classList.contains("open"))
+        dropdownmenu1.classList.add("open");
+      else dropdownmenu1.classList.remove("open");
+    }
   }
 });
 
 diff2.addEventListener("click", () => {
-  if (dropdownmenu1) {
-    if (dropdownmenu1.classList.contains("open"))
-      dropdownmenu1.classList.remove("open");
-  }
-  if (dropdownmenu2) {
-    if (!dropdownmenu2.classList.contains("open"))
-      dropdownmenu2.classList.add("open");
-    else dropdownmenu2.classList.remove("open");
+  if (restartBtn.classList.contains("hide")) {
+    if (dropdownmenu1) {
+      if (dropdownmenu1.classList.contains("open"))
+        dropdownmenu1.classList.remove("open");
+    }
+    if (dropdownmenu2) {
+      if (!dropdownmenu2.classList.contains("open"))
+        dropdownmenu2.classList.add("open");
+      else dropdownmenu2.classList.remove("open");
+    }
   }
 });
 
@@ -53,10 +68,32 @@ options.forEach((option) => {
       option.classList.add("selected");
     difficultyText.innerHTML = optionValue.innerHTML.trim();
     dropdownmenu.classList.remove("open");
+    //update the text
+    updateText(difficultyText.textContent.toLowerCase());
   });
 });
 
+window.onload = function () {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      easyTexts = data.easy.map((item) => item.text);
+      mediumTexts = data.medium.map((item) => item.text);
+      hardTexts = data.hard.map((item) => item.text);
+
+      updateText("easy");
+    });
+};
+
+function updateText(text) {
+  const textElement = document.querySelector(".text");
+  if (text === "easy") textElement.innerHTML = easyTexts[easyNum];
+  else if (text === "medium") textElement.innerHTML = mediumTexts[mediumNum];
+  else if (text === "hard") textElement.innerHTML = hardTexts[hardNum];
+}
+
 document.addEventListener("click", function (event) {
+  //  1- drop down menus
   const insideCCombo1 = event.target.closest(".custom-combo");
 
   if (!insideCCombo1) {
@@ -69,4 +106,20 @@ document.addEventListener("click", function (event) {
         dropdownmenu2.classList.remove("open");
     }
   }
+});
+textContainer.addEventListener("click", () => {
+  if (
+    !dropdownmenu1.classList.contains("open") &&
+    !dropdownmenu2.classList.contains("open")
+  ) {
+    start.classList.add("hide");
+    text.classList.remove("filter");
+    restartBtn.classList.remove("hide");
+  }
+});
+
+restartBtn.addEventListener("click", () => {
+  start.classList.remove("hide");
+  text.classList.add("filter");
+  restartBtn.classList.add("hide");
 });
